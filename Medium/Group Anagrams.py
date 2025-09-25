2d71613eaed3ae247cdc09eabaa0fb690dfb8294
Hans 展豪 <hansvalerie19@gmail.com>
2025-09-25 12:47:56 +0700

    Add files via upload

--- Medium/Group Anagrams.py
+++ Medium/Group Anagrams.py
@@ -1,1 +1,21 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        sorts = []
+        group = {}
+        for word in strs:
+            sorts.append("".join(sorted(word)))
+            
+        for i, j in zip(strs, sorts):
+            if j not in group:
+                group[j] = [i]
+            else :
+                group[j].append(i)
+
+        return list(group.values())
+
+            
 
